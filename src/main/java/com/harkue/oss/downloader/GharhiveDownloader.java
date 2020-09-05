509f708664251468f79f0e58395acfeee42fa266
harkue <harkue@hotmail.com>
2020-09-05 11:47:34 +0800

    add proxy setting

--- src/main/java/com/harkue/oss/downloader/GharhiveDownloader.java
+++ src/main/java/com/harkue/oss/downloader/GharhiveDownloader.java
@@ -2,10 +2,12 @@ package com.harkue.oss.downloader;
 
 import com.harkue.oss.utils.OssFileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,9 +21,20 @@ public class GharhiveDownloader {
     public static final String GHARCHIVE_BASE_URL = "https://data.gharchive.org/";
 
     public void download(String filename) {
+        this.download(filename, false);
+    }
+
+    public void download(String filename, boolean useProxy) {
         String url = GHARCHIVE_BASE_URL + filename;
 
         CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        if (useProxy) {
+            HttpHost proxy = new HttpHost("127.0.0.1", 3128);
+            DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
+            httpclient = HttpClients.custom().setRoutePlanner(routePlanner).build();
+        }
+
         try {
             HttpGet httpget = new HttpGet(url);
             httpget.setConfig(RequestConfig.copy(RequestConfig.DEFAULT).setConnectTimeout(240000).build());
